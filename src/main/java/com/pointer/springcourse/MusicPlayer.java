2818a3613405c10fb1ad218a03063b4db232b355
Volodymyr00 <istoria1918@gmail.com>
2020-04-27 16:01:20 +0300

    Added List of Music

--- src/main/java/com/pointer/springcourse/MusicPlayer.java
+++ src/main/java/com/pointer/springcourse/MusicPlayer.java
@@ -1,11 +1,18 @@
 package com.pointer.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-
-    private Music music;
     private String name;
     private int volume;
+
+    private List<Music> musicList = new ArrayList<>();
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public String getName() {
         return name;
@@ -23,18 +30,8 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public void playMusicList() {
+        for (Music music : musicList)
+            System.out.println("Playing: " + music.getSong());
     }
 }
